445923e160b0addf4da3532e38fa23975f8aae3c
Justin B <brewer051@gmail.com>
2018-06-06 03:46:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,8 @@
 def display_rainbow(["red", "orange", "yellow", "green" "blue", "indigo", "violet"])
-  puts R:[0]
-  puts O:[1]
-  puts Y:[2]
-  puts G:[3]
+  puts R:"[0]"
+  puts O:"[1]"
+  puts Y:"[2]"
+  puts G:"[3]"
   puts B:[4]
   puts I:[5]
   puts V:[6]
